5502e9f8072b06e4750923a36655a0c9b0fe3a32
Michał Skupiński <skupinski420@outlook.com>
2025-02-14 15:39:05 +0100

    query params dict added instead of hardcoding query params

--- telegram_api_client.py
+++ telegram_api_client.py
@@ -5,9 +5,16 @@ import requests
 # res = requests.get(url)
 # print(res._content)
 
+
 def send_text(TOKEN, chat_id, text):
 
     url = f"https://api.telegram.org/bot{TOKEN}/sendMessage?chat_id={chat_id}&text={text}"
     response = requests.get(url)
 
     return response.status_code
+
+    # check if this would work
+    # params = {'chat_id':chat_id, 'text':text}
+    # url = 'https://api.telegram.org/bot{TOKEN}/sendMessage'
+    # response = requests.get(url, params=params)
+    # print(response.url)
